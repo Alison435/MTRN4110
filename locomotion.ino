a8ea57769f5f8421c35d3164ac1097592ec6a5a1
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-06-12 21:16:58 +1000

    using static function syntax

--- locomotion.ino
+++ locomotion.ino
@@ -11,52 +11,34 @@
 // Avoids prepending scope operator to all functions
 using namespace hardware;
 
-//Note: The .cpp file has explicit instantiations
-// -Instantiating objects for each desired class-
-
-// Tests for Phase A demonstrations
-#define USE_BLINK
-//#define USE_FADE
+//#define USE_BLINK
+#define USE_FADE
 //#define USE_ATEST //TODO!
 //#define MOTOR_TEST //TODO!
 //#define ENCODER_TEST //TODO!
 
-// i) objects for pin I/O
-led onboardLed;
-statusRed ledR;
-statusGreen ledG; //used for fade pwm_write test
-analogTest aTest; 
+void setupDigital()
+{
+  led::config_io_mode(io_mode::output);
+  statusRed::config_io_mode(io_mode::output);
+  statusGreen::config_io_mode(io_mode::output);
+}
 
-//(error with pins:: having undefined reference)
-// ii) objects for motors 
-//right_motor rMotor;
-pins::en1 speedR; //enable pin for motor (h-bridge)
-pins::in1 dir1;
-pins::in2 dir2;
-
-// iii) units
-//units::microseconds microCheck; 
-//units::milliseconds millisCheck;
+void setupMotor()
+{  
+  in1::config_io_mode(io_mode::output); 
+  in2::config_io_mode(io_mode::output); 
+  in3::config_io_mode(io_mode::output); 
+  in4::config_io_mode(io_mode::output); 
+  //right_motor::enable();
+  //left_motor::enable();
+}
 
 void setup() {
 
   Serial.begin(115200);
-  
-  // a) set up for digital I/O pins (make into function)
-  onboardLed.config_io_mode(io_mode::output);
-  ledR.config_io_mode(io_mode::output);
-  ledG.config_io_mode(io_mode::output);
-
-  // b) set up for motor: (make into function)  
-  
-  //pwm control for right motor terminal
-  //speedR.config_io_mode(io_mode::output);
-  //rMotor.enable()
-     
-  //dir1 and dir2 on right motor terminal
-  //dir1.config_io_mode(io_mode::output); 
-  //dir2.config_io_mode(io_mode::output); 
-  
+  setupDigital();
+  setupMotor(); 
 }
 
 void loop() {
@@ -65,51 +47,48 @@ void loop() {
 
   #ifdef USE_BLINK
     //Blink test using API
-    ledG.high();
-    ledR.low();      
-    //microCheck.now();
-    delay(1000);
+    statusRed::high();
+    statusGreen::low();      
+    delay(500);
     
-    ledG.low();
-    ledR.high();      
-    //millisCheck.now();
-    delay(1000);
+    statusRed::low();
+    statusGreen::high();      
+    delay(500);
   #endif
   
   #ifdef USE_FADE
     //Fade test using API on Green LED @ PWM pin #3
-    ledG.pwm_write(25.0);
+    statusGreen::pwm_write(); //argument of form units::percentage duty_cycle
     delay(50);
-    ledG.pwm_write(50.0);
+    statusGreen::pwm_write(50.0);
     delay(50);
-    ledG.pwm_write(75.0);
+    statusGreen::pwm_write(75.0);
     delay(50);
-    ledG.pwm_write(100.0);
+    statusGreen::pwm_write(100.0);
     delay(50);
   #endif
   
   //2. Analog I/O Functionality
 
   #ifdef ATEST
-    double analogVal = aTest.analog_read();
+    double analogVal = analogPin::analog_read();
     Serial.println(analogVal);
   #endif
   
   //3. Motor Control Functionality
 
   #ifdef MOTOR_TEST
-    rMotor.forward();
+    right_motor::forward()
     delay(100);
-    rMotor.backward();
+    right_motor::backward();
     delay(100);
-    //speedR.pwm_write(50.0); //50% speed
   #endif
 
   //4. Read Wheel Encoder + Distance Travelled
 
   #ifdef ENCODER_TEST
     //test encoder count/direction
-    
+    //encoder::
   #endif
 
 
