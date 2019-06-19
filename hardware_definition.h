a83e508f845481d07bf195610bcfb51d0f4cf821
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-06-19 14:37:21 +1000

    commented in MEGA pins

--- hardware_definition.h
+++ hardware_definition.h
@@ -17,36 +17,36 @@ namespace hardware
         using imu_interrupt = digital_pin<1U>;
 
         // The sonar_trigger is ultrasound trigger pin
-        using sonar_trigger = digital_pin<6U>;
+        using sonar_trigger = digital_pin<6U>; //<digital_pin<12U>
 
         // The ultrasound_echo is ultrasound echo pin
-        using sonar_echo = digital_pin<7U>;
+        using sonar_echo = digital_pin<7U>; //<digital_pin<13U>;
 
-        using left_lidar_enable = digital_pin<12U>;
-        using right_lidar_enable = digital_pin<13U>;
+        using left_lidar_enable = digital_pin<12U>; //<digital_pin<20U>; //SDA on Mega
+        using right_lidar_enable = digital_pin<13U>; //<digital_pin<21U>; //SCL on Mega
 
-        constexpr auto left_lidar_address = 0x22;
+        constexpr auto left_lidar_address = 0x22; //TODO: check i2C addresses
         constexpr auto right_lidar_address = 0x42;
+        constexpr auto imu_lidar_address = 0x68;
 
         // 2) Pins for Motor/Encoder
 
-        // Right Motor (Direction and Speed)
-        using in1 = digital_pin<2U>;
-        using in2 = digital_pin<4U>;
-        //using en1 = digital_pin<3U>;
+        // Right Motor (Direction)
+        using in1 = digital_pin<7U>; //<digital_pin<8U>;
+        using in2 = digital_pin<8U>; //<digital_pin<9U>;
 
-        // Left Motor (Direction and Speed)
-        using in3 = digital_pin<9U>;
-        using in4 = digital_pin<10U>;
-        //using en2 = digital_pin<12U>;
+        // Left Motor (Direction)
+        using in3 = digital_pin<11U>; //<digital_pin<12U>;
+        using in4 = digital_pin<12U>; //<digital_pin<13U>;
 
         // Encoder interrupt pins
         using left_encoder_a = digital_pin<2U>; //<digital_pin<18U>;
-        using right_encoder_a = digital_pin<3U>; //digital_pin<19U>;
+        using left_encoder_b = digital_pin<1U>; //<digital_pin<22U>;
 
-        // Encoder digital pins
-        using left_encoder_b = digital_pin<21U>;
-        using right_encoder_b = digital_pin<23U>;
+        using right_encoder_a = digital_pin<3U>; //digital_pin<19U>;
+        using right_encoder_b = digital_pin<4U>; //<digital_pin<24U>;
+
+
 
     }    // namespace pins
 
@@ -56,24 +56,23 @@ namespace hardware
 using led = digital_pin<13U>;
 
 // Red LED
-using statusRed = digital_pin<8U>;
+using statusRed = digital_pin<8U>; //<digital_pin<2U>;
 
 // Green LED
-using statusGreen = digital_pin<6U>;
-
-// AnalogTest A1
-using analogPin = analog_pin<digital_pin<1U>>;
+using statusGreen = digital_pin<6U>; //<digital_pin<3U>;
 
 // 2. Pins for Encoder, Motor and Wheel
+
+using right_motor = motor<pins::in1, pins::in2>;
+using left_motor = motor<pins::in3, pins::in4>;
+using en1 = digital_pin<9U>; //used for motor pwm //<digital_pin<10U>;
+using en2 = digital_pin<10U>; //used for motor pwm //<digital_pin<11U>;
 
 using left_wheel = wheel<pins::left_encoder_a, pins::left_encoder_b>;
 using right_wheel = wheel<pins::right_encoder_a, pins::right_encoder_b>;
 
 using left_encoder = encoder<pins::left_encoder_a, pins::left_encoder_b>;
 using right_encoder = encoder<pins::right_encoder_a, pins::right_encoder_b>;
-
-using right_motor = motor<pins::in1, pins::in2>;
-using left_motor = motor<pins::in3, pins::in4>;
 
 // 3. Pins for Sensors
 
@@ -89,5 +88,7 @@ using serial = serial_api<serial_tag<0>>;
 
 // The serial is used to communicate with Bluetooth module.
 using bluetooth = serial_api<serial_tag<1>>;
+
+// Pins for LCD
 
 }    // namespace hardware
