f1edb0b22719a4cda524d18c73a7526870a6f980
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-07-09 18:14:10 +1000

    create exploreTest,py

---
+++ exploreTest.py
@@ -0,0 +1,50 @@
+# Manual Python control over robot
+# Used to test exploration 
+
+# Test script to send commands over Bluetooth
+
+import serial
+import time
+
+COM_MOUSE = "COM31" #After pairing bluetooth HC06 module with computer
+#check COM port
+
+def main():
+
+    # Establish serial connection COM31 through Bluetooth
+    ser = serial.Serial(COM_MOUSE, 9600, timeout = 50)
+
+    # Wait for Arduino to be reset
+    time.sleep(2)
+
+# User input for manual control
+    while (1):
+
+        action = input("Press 1 (straight), 2 (left), 3 (right)\n")
+
+        if (action == 1):
+
+            # Send 1 to make robot drive
+            ser.write('s\n')
+
+        elif (action == 2):
+
+            # Send 2 to activate left turn on spot
+            ser.write('l\n')
+
+        elif( action == 3):
+
+            # Send 3 to activate right turn on spot
+            ser.write('r\n')
+
+        else:
+
+            print("Invalid Input")
+
+#----------Execute MAIN Here-----------
+
+main()
+
+if __name__ == "__main__":
+    main()
+
