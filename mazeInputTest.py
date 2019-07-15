deb506eb1897ad996e8e8d86f19eae9e32de7ecc
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-07-15 11:58:29 +1000

    Update mazeInputTest.py

--- mazeInputTest.py
+++ mazeInputTest.py
@@ -1,32 +1,28 @@
 # Test Script to send detected horizontal and vertical
 # walls to Flood Fill algorithm on Arduino
+# When PhaseB_CV_Planing.ino is loaded onto Arduino
 
 import serial
 import time
 
-COM_MOUSE = "COM7" #change this to match your COM port (bluetooth port)
+COM_MOUSE = "COM31" #change this to match your COM port (bluetooth port)
 
 def main():
 
     print("Sending vertical array\n")
+    #Maze as of 15/7/19 (red chair cell is [0,0])
     # Send vertical array
-    ser.write("10001000011110010111100122111110001222211010001011\n")
-    print ser.readline() # Read the newest output from the Arduino
+    ser.write("10100000011010000001101010010110101001111010001001\n")
+    #print ser.readline() # Read the newest output from the Arduino
+    print("Sent vertical array\n")
 
     time.sleep(0.5)
 
     print("Sending horizontal array\n")
     # Send horizontal array
-    ser.write("111111111001101122010111221101101222010101010111111111\n")
-    print ser.readline() # Read the newest output from the Arduino
-
-    # Prints the results of the inputted arrays
-    print ser.readline()
-    print ser.readline()
-    print ser.readline()
-    print ser.readline()
-    print ser.readline()
-    print ser.readline() # Should be COMMANDS FOR ROBOT MOVEMENT:...
+    ser.write("111111111011111111101000010010100001101011100111111111\n")
+    #print ser.readline() # Read the newest output from the Arduino
+    print("Sent horizontal array\n")
 
 #----------Execute MAIN Here-----------
 
