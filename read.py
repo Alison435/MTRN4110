445c81dacc5370dcdc8ff8a7867ae7148aef3c69
dahongable <52072942+dahongable@users.noreply.github.com>
2019-07-25 18:00:37 +1000

    Add files via upload

---
+++ read.py
@@ -0,0 +1,22 @@
+import time
+import serial
+
+COM_MOUSE = "COM10"
+
+def main():
+	time.sleep(1)
+	ser = serial.Serial(COM_MOUSE,9600,timeout = 20)
+
+	f=open("bot.txt", "r")
+	#contents =f.read() #read file data and store it in variable content 
+
+	f1 = f.readlines()
+
+	for x in f1:
+		print(x),
+		ser.write(x)
+		time.sleep(1.5)
+	print("\n")	
+
+
+main()
